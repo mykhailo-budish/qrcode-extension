3ff84118cfe9d4b557d65f245459044f50d5b52c
Mykhailo Budish <budish.mikhail@globaldev.tech>
2025-07-13 19:40:20 +0300

    Version 0.2: update QR code image extraction from tab image to consider window.devicePixelRatio

--- src/image-scrapper.js
+++ src/image-scrapper.js
@@ -8,20 +8,28 @@ function scrapImage({ tabImageUrl, imageUrl }) {
     const image = new Image();
     image.src = tabImageUrl;
     image.onload = () => {
+      const scale = window.devicePixelRatio;
       const canvas = document.createElement("canvas");
-      canvas.width = imgBoundingRect.width;
-      canvas.height = imgBoundingRect.height;
       const context = canvas.getContext("2d");
+
+      const subImageX = imgBoundingRect.x * scale;
+      const subImageY = imgBoundingRect.y * scale;
+      const subImageWidth = imgBoundingRect.width * scale;
+      const subImageHeight = imgBoundingRect.height * scale;
+
+      canvas.width = subImageWidth;
+      canvas.height = subImageHeight;
+
       context.drawImage(
         image,
-        imgBoundingRect.x,
-        imgBoundingRect.y,
-        imgBoundingRect.width,
-        imgBoundingRect.height,
+        subImageX,
+        subImageY,
+        subImageWidth,
+        subImageHeight,
         0,
         0,
-        imgBoundingRect.width,
-        imgBoundingRect.height
+        subImageWidth,
+        subImageHeight
       );
       const clippedImageUrl = canvas.toDataURL('image/png');
       res(clippedImageUrl);
